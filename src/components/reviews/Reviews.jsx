1d1f49d610eb7957f7c0fb9e07989ee43113d5af
Anton Vitvitskii <avitvitskii@flightpath3d.com>
2025-01-27 11:08:27 +0300

    Implements Review component

--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -1,10 +1,14 @@
+import { Review } from "../review/review";
+
 export const Reviews = ({ reviews }) => {
   return (
     <>
       <h3>Отзывы</h3>
       <ul>
         {reviews.map((review) => (
-          <li key={review.id}>{review.text}</li>
+          <li key={review.id}>
+            <Review text={review.text} />
+          </li>
         ))}
       </ul>
     </>
