d5af4a40f28ff01481e319e799e944c5f1c25c86
Anton Vitvitskii <avitvitskii@flightpath3d.com>
2025-01-31 13:12:49 +0300

    Improves logic for selecting active restaurant

--- src/components/restaurants/Restaraunts.jsx
+++ src/components/restaurants/Restaraunts.jsx
@@ -3,17 +3,27 @@ import { restaurants } from "../../mock";
 import { Restaurant } from "../restaurant/Restaurant";
 
 export const Restaurants = () => {
-  const [restaurant, setRestaurant] = useState(restaurants[0]);
+  const [activeRestaurantId, setActiveRestaurantId] = useState(
+    restaurants[0].id
+  );
+
+  const activeRestaurant = restaurants.find(
+    (restaurant) => restaurant.id === activeRestaurantId
+  );
 
   return (
     <>
       <h1>Restaurants</h1>
       {restaurants.map((restaurant) => (
-        <button key={restaurant.id} onClick={() => setRestaurant(restaurant)}>
+        <button
+          key={restaurant.id}
+          onClick={() => setActiveRestaurantId(restaurant.id)}
+          disabled={restaurant.id === activeRestaurantId}
+        >
           {restaurant.name}
         </button>
       ))}
-      <Restaurant restaurant={restaurant} />
+      <Restaurant restaurant={activeRestaurant} />
     </>
   );
 };
