8a6de7e245f0bda9d412b8075e3ee4fd349a268f
Anton Vitvitskii <avitvitskii@flightpath3d.com>
2025-03-18 17:02:57 +0300

    Makes only NavLink for Tablink removing Button

--- src/components/tablink/Tablink.jsx
+++ src/components/tablink/Tablink.jsx
@@ -1,10 +1,18 @@
 import { NavLink } from "react-router";
-import { Button } from "../button/Button";
+import classNames from "classnames";
+import styles from "./tablink.module.css";
 
 export const Tablink = ({ link, title }) => {
   return (
-    <NavLink to={link}>
-      {({ isActive }) => <Button title={title} disabled={isActive} />}
+    <NavLink
+      to={link}
+      className={({ isActive }) =>
+        classNames(styles.navlink, {
+          [styles.active]: isActive,
+        })
+      }
+    >
+      {title}
     </NavLink>
   );
 };
